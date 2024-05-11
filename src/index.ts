099c28aefa1fb2aeb53368f482c304ac9e56bc83
Luís Fritzen <pmi_lfritzen@uolinc.com>
2024-05-11 15:27:39 -0300

    refactor: try to configure vercel deployment

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,12 @@ import { assetRouter } from "./routes/assetRouter.js";
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
 app.use("/assets", assetRouter);
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
